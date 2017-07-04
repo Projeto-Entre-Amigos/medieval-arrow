2a3a1e803fa0143d95f94c48d5d92a86205f5a09
Fernando da Silva Sousa <wild.nando@gmail.com>
2017-07-04 02:53:52 -0300

    refatora ação de pulo

--- src/player.gd
+++ src/player.gd
@@ -1,18 +1,14 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var jumping = false;
-
 func _ready():
 	set_process(true)
-	# Called every time the node is added to the scene.
-	# Initialization here
+
 func _process(delta):
-	if (Input.is_action_pressed("jump") && !jumping):
-		set_linear_velocity(Vector2(0,-100))
-		jumping = true
-	if (get_linear_velocity() == Vector2 (0,0)):
-		jumping = false
-	print (get_linear_velocity())
+	if (Input.is_action_pressed("jump") && !is_jumping()):
+		jump ()
+
+func jump (height = 100):
+	set_linear_velocity(Vector2(0,-100))
+
+func is_jumping ():
+	return (get_linear_velocity() != Vector2 (0,0))

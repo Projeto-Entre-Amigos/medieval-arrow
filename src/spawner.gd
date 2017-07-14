0424018bb4a7bd739d83c92f0cdeba8e618552c2
Fernando da Silva Sousa <wild.nando@gmail.com>
2017-07-14 14:50:57 -0300

    remote quantidade adiciona PackagedScene às propriedades

--- src/spawner.gd
+++ src/spawner.gd
@@ -1,26 +1,21 @@
 extends Node2D
 
-# Instancia objeto em tempo aleatorio
-# Instancia numero aleatorio de objetos
-# Deleta objetos assim que saem do escopo(transcendem os limites da tela)
+export(PackedScene) var spawn
+onready var spawnref = load (spawn.get_path())
 
 export(float) var min_intervalo = 0
 export(float) var max_intervalo = 10
-export(int) var min_quantidade = 0
-export(int) var max_quantidade = 10
 
-var spawn = preload ("../scene/player.tscn")
 onready var timer = get_child(0)
 
 func _ready():
 	randomize()
 	timer.set_wait_time(rand_range(min_intervalo, max_intervalo))
-	timer.connect("timeout", self, "_spawn")
 	print ("Iniciando spawner")
 
 func _spawn():
-	# Para cada inimigo novo criamos uma instancia
-	var spawned = spawn.instance ()
-	add_child(spawned)
-	# Coloca novo valor para criação dos inimigos
+	add_child(spawnref.instance ())
 	timer.set_wait_time(rand_range(min_intervalo, max_intervalo))
+
+func _on_timer_timeout():
+	_spawn ()

fd2106c3f6a4a9b357e88f3e438055db802e957b
Fernando da Silva Sousa <wild.nando@gmail.com>
2017-07-14 01:49:26 -0300

    implementa spawn de cenas

--- src/spawner.gd
+++ src/spawner.gd
@@ -4,12 +4,12 @@ extends Node2D
 # Instancia numero aleatorio de objetos
 # Deleta objetos assim que saem do escopo(transcendem os limites da tela)
 
-export(Object) var spawn
 export(float) var min_intervalo = 0
 export(float) var max_intervalo = 10
 export(int) var min_quantidade_inimigos = 0
 export(int) var max_quantidade_inimigos = 10
 
+var spawn = preload ("../scene/player.tscn")
 onready var timer = get_child(0)
 
 func _ready():
@@ -20,7 +20,7 @@ func _ready():
 
 func _spawn():
 	# Para cada inimigo novo criamos uma instancia
-	var spawned = spawn.new ()
-	print ("Fui chamado")
+	var spawned = spawn.instance ()
+	add_child(spawned)
 	# Coloca novo valor para criação dos inimigos
 	timer.set_wait_time(rand_range(min_intervalo, max_intervalo))

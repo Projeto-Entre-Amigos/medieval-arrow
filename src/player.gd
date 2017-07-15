be3209721f0c2795cd7b3bd70566f12aec783513
Gabriel Azevedo <gabriel.laxus@gmail.com>
2017-07-14 22:55:02 -0300

    Add Pulo
    adiciona animação de pulo ao script

--- src/player.gd
+++ src/player.gd
@@ -2,13 +2,16 @@ extends RigidBody2D
 
 func _ready():
 	set_process(true)
-
+	
 func _process(delta):
 	if (Input.is_action_pressed("jump") && !is_jumping()):
 		jump ()
-
-func jump (height = 100):
-	set_linear_velocity(Vector2(0,-100))
-
+		get_node("anim").play("jumping")
+	
+	 
+func jump (height = 70):
+	set_linear_velocity(Vector2(0,-70))
+	
+	
 func is_jumping ():
 	return (get_linear_velocity() != Vector2 (0,0))

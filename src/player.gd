d77664ff8d07af307c77b5acc72e7e4760ac6c88
Fernando da Silva Sousa <wild.nando@gmail.com>
2017-07-15 01:08:51 -0300

    usa parametros padrão da função

--- src/player.gd
+++ src/player.gd
@@ -6,12 +6,10 @@ func _ready():
 func _process(delta):
 	if (Input.is_action_pressed("jump") && !is_jumping()):
 		jump ()
-		get_node("anim").play("jumping")
-	
-	 
+ 
 func jump (height = 70):
-	set_linear_velocity(Vector2(0,-70))
-	
-	
+	set_linear_velocity(Vector2(0,-height))
+	get_node("anim").play("jumping")
+
 func is_jumping ():
 	return (get_linear_velocity() != Vector2 (0,0))

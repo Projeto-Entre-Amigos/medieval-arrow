49f9cff7a9744dcc88c1cdb0c849a5e0afb2ed9e
Fernando da Silva Sousa <wild.nando@gmail.com>
2017-08-03 12:55:14 -0300

    Iniciando tratamento de colisões no inimigo

--- src/enemy.gd
+++ src/enemy.gd
@@ -5,3 +5,7 @@ export(float) var walk_speed
 func _integrate_forces(state):
 	var lv_y = get_linear_velocity().y
 	set_linear_velocity(Vector2(-walk_speed, lv_y))
+
+func _on_enemy_body_enter_shape( body_id, body, body_shape, local_shape ):
+	print ("Body has been hit ", body_id)
+	print (body.get_type ())
